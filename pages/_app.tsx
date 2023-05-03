f8c6461785f6c3892fbe441403f700041f2d10fd
Jason Zhuang <jason71708@gmail.com>
2023-05-03 10:27:07 +0800

    feat: add page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from 'next/app'
 import 'styles/globals.css'
+import PageWithTransition from "components/transitions/WithTransition";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return <PageWithTransition
+    Component={Component}
+    pageProps={pageProps}
+    router={router}
+  />
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,38 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./app/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideUpEnter: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(20px)",
+          },
+          "100%": {
+            opacity: 100,
+            transform: "translateY(0px)",
+          },
+        },
+        slideUpLeave: {
+          "0%": {
+            opacity: 100,
+            transform: "translateY(0)",
+          },
+          "100%": {
+            opacity: 0,
+            transform: "translateY(20px)",
+          },
+        },
+      },
+      animation: {
+        slideUpEnter: "slideUpEnter .3s ease-in-out",
+        slideUpLeave: "slideUpLeave .3s ease-in-out",
+      },
+    },
   },
   plugins: [],
-}
+};
